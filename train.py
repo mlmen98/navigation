8b97965c8b46e1f34ffdf98220f74021de3782e5
mlmen98 <songguihaoasd407@163.com>
2020-12-08 18:54:02 +0800

    update

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ from data_utils.dataset_util import input_fn
 
 
 def main(unused_argv):
-  # Using the Winograd non-fused algorithms provides a small performance boost.
+  # Using the Winograd non-fused algorithm provides a small performance boost.
   os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
 
   if TrainingConfig.clean_model_dir:
@@ -60,10 +60,11 @@ def main(unused_argv):
     model.train(
         input_fn=lambda: input_fn(True, ModelConfig.data_dir, TrainingConfig.batch_size, TrainingConfig.epochs_per_eval),
         hooks=train_hooks,
-        # steps=1  # For debug
+        # steps=1  # For debugging
     )
 
     tf.logging.info("Start evaluation.")
+    
     # Evaluate the model and print results
     eval_results = model.evaluate(
         # Batch size must be 1 for testing because the images' size differs
